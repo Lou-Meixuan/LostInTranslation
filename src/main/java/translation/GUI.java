053316c108dbce3a18efc7b5522bcc5e06679b54
Lou-Meixuan <meixuan.lou@mail.utoronto.ca>
2025-09-24 19:51:39 -0400

    delete the submit button and change the format of Task D and deleted three unused import statement

--- src/main/java/translation/GUI.java
+++ src/main/java/translation/GUI.java
@@ -1,11 +1,8 @@
 package translation;
 
 import javax.swing.*;
-import javax.swing.event.ListSelectionEvent;
-import javax.swing.event.ListSelectionListener;
 import java.awt.*;
 import java.awt.event.*;
-import java.util.Arrays;
 
 public class GUI {
 
